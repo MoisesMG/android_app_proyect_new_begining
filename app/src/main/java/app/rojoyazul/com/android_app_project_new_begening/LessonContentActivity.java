4f0e58f847275b8442878272db797167b3cf13f2
MoisesMG <menagonzalezmoises@gmail.com>
2016-12-04 22:23:53 -0600

    eventos onClick agregados a LessonSelectionActivity.java

---
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/LessonContentActivity.java
@@ -0,0 +1,13 @@
+package app.rojoyazul.com.android_app_project_new_begening;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LessonContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_content);
+    }//fin del metodo
+}//fin de la clase
