cd316d0dfa395bb8e12e1122966d4cca104489a3
MoisesMG <menagonzalezmoises@gmail.com>
2016-12-08 05:28:18 -0600

    actividades de los botones implementadas con exito en todas

--- app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/CreditsActivity.java
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/CreditsActivity.java
@@ -9,5 +9,20 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-    }
-}
+
+        //TODO agregar logotipo del equipo
+        //TODO seccion de "acerca de la aplicacion y el equipo de desarrollo
+        //TODO agregar agradecimientos
+        //TODO agregar informacion de los desarrolladores (fotos)
+
+        /** establecer icono en el action bar**/
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher1);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
+
+        /**************************************/
+
+    }//fin del metodo
+}//fin de la clase
--- app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/LessonSelectionActivity.java
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/LessonSelectionActivity.java
@@ -164,11 +164,13 @@ public class LessonSelectionActivity extends AppCompatActivity implements Observ
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_credits:
-                Toast.makeText(getApplicationContext(), "se presiono el boton de creditos", Toast.LENGTH_SHORT);
+                Intent i = new Intent(LessonSelectionActivity.this, CreditsActivity.class);
+                startActivity(i);
                 return true;
 
             case R.id.action_suggestion:
-                Toast.makeText(getApplicationContext(), "se presiono el boton de sugerencias", Toast.LENGTH_LONG);
+                Intent in = new Intent(LessonSelectionActivity.this, SuggestionsActivity.class);
+                startActivity(in);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
