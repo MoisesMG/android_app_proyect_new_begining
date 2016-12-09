aff31da990ac5f637c62c734f4bcb5697d4db7e0
MoisesMG <menagonzalezmoises@gmail.com>
2016-12-09 01:41:24 -0600

    busqueda por id implementada

--- app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/CreditsActivity.java
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/CreditsActivity.java
@@ -1,19 +1,45 @@
 package app.rojoyazul.com.android_app_project_new_begening;
 
+import android.net.Uri;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class CreditsActivity extends AppCompatActivity {
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
 
+import java.util.Map;
+
+public class CreditsActivity extends AppCompatActivity implements ObservableScrollViewCallbacks{
+    Firebase creditsRef = new Firebase("https://androidappprojectnewbegening.firebaseio.com/credits");
+    TextView creditsTitle, creditsRead, goals, thanks;
+    SimpleDraweeView logoTeam;
+    Uri logoTeamUrlImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
 
-        //TODO agregar logotipo del equipo
-        //TODO seccion de "acerca de la aplicacion y el equipo de desarrollo
-        //TODO agregar agradecimientos
-        //TODO agregar informacion de los desarrolladores (fotos)
+
+        /********* efecto del observable scroll view ***************/
+        ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.activity_credits);
+        scrollView.setScrollViewCallbacks(this);
+        /************************************************/
+
+        /*** buscar por id ****/
+        creditsTitle = (TextView) findViewById(R.id.creditsTitle);
+        creditsRead = (TextView) findViewById(R.id.creditsRead);
+        goals = (TextView) findViewById(R.id.goals);
+        thanks = (TextView) findViewById(R.id.thanks);
+        logoTeam = (SimpleDraweeView) findViewById(R.id.logoTeam);
+        /***********************/
 
         /** establecer icono en el action bar**/
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -21,8 +47,48 @@ public class CreditsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
-
         /**************************************/
 
+        /********* recuperar datos de firebase ************/
+        creditsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+            Map<String, String> mapValues = dataSnapshot.getValue(Map.class);
+            }//fin del metodo
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }//fin del metodo
+        });
+        /******************************************/
+
+    }//fin del metodo
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (ab == null) {
+            return;
+        }
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
     }//fin del metodo
 }//fin de la clase
