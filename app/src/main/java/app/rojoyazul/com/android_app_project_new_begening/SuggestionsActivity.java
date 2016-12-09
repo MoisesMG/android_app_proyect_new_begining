9b2474fe77a20afe21ee5655e53c752362609b63
MoisesMG <menagonzalezmoises@gmail.com>
2016-12-09 05:01:27 -0600

    Iniciar la siguiente caractetistica

--- app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/SuggestionsActivity.java
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/SuggestionsActivity.java
@@ -3,12 +3,18 @@ package app.rojoyazul.com.android_app_project_new_begening;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
 
 public class SuggestionsActivity extends AppCompatActivity implements ObservableScrollViewCallbacks{
+    EditText mName, mEmail, mSuggestion;
+    ImageButton mBtnSend, mBtnClear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +22,7 @@ public class SuggestionsActivity extends AppCompatActivity implements Observable
 
         ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.activity_suggestions);
         scrollView.setScrollViewCallbacks(this);
+
         /**********************************************/
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher1);
@@ -24,7 +31,13 @@ public class SuggestionsActivity extends AppCompatActivity implements Observable
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
         /**************************************/
 
-        //TODO agregar insercion de firebase
+        /********* buscar por id ****/
+        mName = (EditText) findViewById(R.id.editTextYourName);
+        mEmail = (EditText) findViewById(R.id.editTextYourEmail);
+        mSuggestion = (EditText) findViewById(R.id.editTextYourSuggets);
+        mBtnSend = (ImageButton) findViewById(R.id.imageButtonSend);
+        mBtnClear = (ImageButton) findViewById(R.id.imageButtonClear);
+        /*************************/
     }//fin del metodo
 
     @Override
