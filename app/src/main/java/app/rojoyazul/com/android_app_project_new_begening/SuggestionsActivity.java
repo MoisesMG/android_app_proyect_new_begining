ad4ef1d94be14d352038d471ed0e216bcc5f0543
MoisesMG <menagonzalezmoises@gmail.com>
2016-12-09 03:53:18 -0600

    actividad de sugerencias aplicada con estilos - sin implementar firebase

--- app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/SuggestionsActivity.java
+++ app/src/main/java/app/rojoyazul/com/android_app_project_new_begening/SuggestionsActivity.java
@@ -1,15 +1,22 @@
 package app.rojoyazul.com.android_app_project_new_begening;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SuggestionsActivity extends AppCompatActivity {
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
 
+public class SuggestionsActivity extends AppCompatActivity implements ObservableScrollViewCallbacks{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestions);
 
+        ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.activity_suggestions);
+        scrollView.setScrollViewCallbacks(this);
+        /**********************************************/
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher1);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
@@ -18,8 +25,30 @@ public class SuggestionsActivity extends AppCompatActivity {
         /**************************************/
     }//fin del metodo
 
-    //TODO agegar logitipo de la aplicacion
-    //TODO agregar peticion de sugerencias
-    //TODO Agregar formulario de sugerencia nombre, correo y sugerencia - boton de enviar y de reseter
-    //TODO guardar sugerencia en la base de datos
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (ab == null) {
+            return;
+        }
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
+    }//fin del metodo
 }//fin de la clase
